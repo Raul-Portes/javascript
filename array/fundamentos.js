89296137318e86fd3a2dec94a9f0344a4729d10c
Raul Portes <raulpx20@gmail.com>
2022-12-14 01:57:40 -0300

    aulas array

---
+++ array/fundamentos.js
@@ -0,0 +1,21 @@
+// trabalhando com fundamentos de array
+
+const lista = ["Pedro", "Paulo","Julia","Maria"]
+
+console.log(lista[0])
+console.log(lista[1])
+console.log(lista[2])
+console.log(lista[3])
+
+console.log("############")
+
+
+// adicionar elemento no array
+lista.push("Raul")
+
+console.log(lista)
+lista[1]="Ciclano" // trocar valor 
+
+console.log(lista)
+lista.sort()
+console.log(lista)
---
+++ array/metodosimportantes.js
@@ -0,0 +1,12 @@
+// trabalhando com metodos importantes 
+
+const lista =["Pedro","Paulo","Alex","julia","Beatriz","Maria"]
+
+lista.pop() // exclui o ultimo elemento do array
+console.log(lista)
+lista.push("Paula") // adiciona o ultimo elemento no array
+console.log(lista)
+lista.shift() // exclui o primeiro elemento do array
+console.log(lista)
+lista.unshift("Bruno")
+console.log(lista) // adiciona o primeiro elemento no array
